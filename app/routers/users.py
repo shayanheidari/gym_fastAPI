8de33ff16ea097b9a6db684c39edff75b20c1195
shayanheidari <shaian.heidari@gmail.com>
2024-12-26 01:54:56 +0330

    added user router

--- app/routers/users.py
+++ app/routers/users.py
@@ -10,7 +10,9 @@ router = APIRouter(prefix="/users", tags=["users"])
 
 
 @router.post("/", response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+def create_user(
+    user: schemas.UserCreate, db: Session = Depends(get_db)
+) -> schemas.UserOut:
     # TODO: hash the password
     new_user = models.User(**user.model_dump())
     db.add(new_user)
@@ -20,7 +22,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/", response_model=List[schemas.UserOut])
-def get_users(db: Session = Depends(get_db)):
+def get_users(db: Session = Depends(get_db)) -> schemas.UserOut:
     users = db.query(models.User).all()
     if not users:
         raise HTTPException(status.HTTP_404_NOT_FOUND,
@@ -29,7 +31,7 @@ def get_users(db: Session = Depends(get_db)):
 
 
 @router.get("/{id}", response_model=schemas.UserOut)
-def get_user(id: int, db: Session = Depends(get_db)):
+def get_user(id: int, db: Session = Depends(get_db)) -> schemas.UserOut:
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
         raise HTTPException(
